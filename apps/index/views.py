6033ec3bcb9ad82ec6f481c9812284965d88d411
andreas <andreaskjl@gmail.com>
2018-12-22 01:33:53 +0100

    add start up files to repo

---
+++ apps/index/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {'hej': 1}
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    context = {'hej': 1}
+    return render(request, 'about.html', context)
+
+
+def contact(request):
+    context = {'hej': 1}
+    return render(request, 'contact.html', context)
+
+
+def news(request):
+    context = {'hej': 1}
+    return render(request, 'news.html', context)
